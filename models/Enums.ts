e5acf5f244b024776f0f28a60bf541f0336dcea3
Ibrahim-Aouni <aouniibrahim94@gmail.com>
2024-12-11 09:07:47 +0100

    first commit

---
+++ models/Enums.ts
@@ -0,0 +1,24 @@
+
+export enum Currency {
+    'eur',
+    'usd',
+    'gbp',
+    'jpy',
+}
+
+export enum OrderStatus {
+    Pending = 'Pending',
+    Completed = 'Completed',
+    Cancelled = 'Cancelled',
+    Shipped = 'Shipped',
+}
+
+
+
+
+
+export enum UserRole {
+    Buyer = 'Buyer',
+    Seller = 'Seller',
+    Both = 'Both',
+}
